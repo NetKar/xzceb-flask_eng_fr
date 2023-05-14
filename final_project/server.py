4d2438becb76e49a02724cb6c23a84123b678dd1
NetKar <netrakarthigeyan@gmail.com>
2023-05-14 03:15:15 -0400

    Final changes

--- final_project/server.py
+++ final_project/server.py
@@ -2,6 +2,17 @@ from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
 import machinetranslation
+import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
+
+AUTHENTICATOR = IAMAuthenticator('6iGJ1ZtElyIn8v_7LTCKE52-Rxkl8PrxRJThQFtGbS0F')
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=AUTHENTICATOR
+)
+language_translator.set_service_url('https://api.jp-tok.language-translator.watson.cloud.ibm.com/instances/8cdb976a-a930-4b45-a634-60110a379304')
 
 app = Flask("Web Translator")
 
@@ -15,8 +26,8 @@ def englishToFrench():
     phrase = list(french_text.values())[0]
     trans = phrase[0]
     tv= list(trans.values())[0]
-    print(tv)
-    return "Translated text to French"
+    print("Translated text to French")
+    return tv
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
@@ -28,11 +39,11 @@ def frenchToEnglish():
     phrase = list(english_text.values())[0]
     trans = phrase[0]
     tv= list(trans.values())[0]
-    print(tv)
-    return "Translated text to English"
+    print("Translated text to English")
+    return tv
 
 @app.route("/")
-def renderIndexPage(res):
-    res.render_template('index.html')
+def renderIndexPage():
+    return render_template('index.html')
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
